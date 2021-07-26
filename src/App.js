715842527b6331e8e015816d287f13a577989823
Jake Armendariz <jakearmendariz99@gmail.com>
2021-07-25 23:16:58 -0600

    changed react header

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   useEffect(() => {
-    document. title = "DEVELOPMENT"
+    document. title = "Jake Armendariz"
   }, [])
 
   return (
